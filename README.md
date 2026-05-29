a463f7e6b89c55276eff38bf19974056b2c840c0
daizhe <daizhe@dhgate.com>
2026-05-29 15:11:21 +0800

    test

--- README.md
+++ README.md
@@ -12,6 +12,7 @@
 # 安装依赖
 npm install
 
+
 # 启动开发服务器
 npm run dev
 ```
--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
+// https://astro.build/configs
 export default defineConfig({
   site: 'https://nomadlifezone.github.io',
   base: '/nomad',
